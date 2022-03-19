d8cbb9872d2942e019af7191ccbfa65c5b755811
mcdzo <maximilianocardozo710@gmail.com>
2022-03-19 15:06:50 -0300

    Add Register page and route with react-router-dom

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,12 @@
+import { Formik } from "formik";
+const Register = () => {
+  return (
+    <div className="w-screen h-screen bg-slate-200 flex flex-col items-center justify-center">
+      <div className="w-1/3 h-5/6 bg-white rounded-md shadow-md flex flex-col items-center justify-center">
+        <div>Crear nuevo usuario</div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
