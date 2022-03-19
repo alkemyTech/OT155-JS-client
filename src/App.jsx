edbec8575746f8c11034ed1c898fbba965250b56
MurabitoF <francomurabito@outlook.com>
2022-03-19 11:13:54 -0300

    Basic interface created to use react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,21 @@
-import { useState } from "react";
-import logo from "./logo.svg";
+import { Route, Routes, Link } from "react-router-dom";
 import "./App.css";
+import Inicio from "./pages";
+import News from "./pages/news";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Hello Vite + React!</p>
-				<p>
-					<button
-						className="bg-slate-500 px-3 py-1 rounded-xl"
-						type="button"
-						onClick={() => setCount((count) => count + 1)}
-					>
-						count is: {count}
-					</button>
-				</p>
-				<p>
-					Edit <code>App.jsx</code> and save to test HMR updates.
-				</p>
-				<p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-					{" | "}
-					<a
-						className="App-link"
-						href="https://vitejs.dev/guide/features.html"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Vite Docs
-					</a>
-				</p>
-			</header>
-		</div>
+		<>
+			<div>
+				<Link to="/">Home</Link>
+				<Link to="/news">News</Link>
+			</div>
+
+			<Routes>
+				<Route path="/" element={<Inicio />} />
+				<Route path="/news" element={<News />} />
+			</Routes>
+		</>
 	);
 }
 
