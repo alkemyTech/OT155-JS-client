c175b9d29d5dab58928414a698400fbc05b75729
MurabitoF <francomurabito@outlook.com>
2022-03-22 13:51:26 -0300

    Cleaning App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import News from "./pages/news";
