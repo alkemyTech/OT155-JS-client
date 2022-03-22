fdbd1330dfb0cfc59f755e2c0198da6555ac83d4
Lucia Bagnis <luciabagnis@gmail.com>
2022-03-22 11:18:05 -0300

    Alert service

--- src/helpers/AlertService.js
+++ src/helpers/AlertService.js
@@ -1,38 +1,61 @@
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
-//Error Card
-const Card = Swal.mixin({
+function errorCardAler(msg) {
+  const errorCard = Swal.mixin({
     toast: true,
     showCancelButton: true,
     timer: 3000,
     timerProgressBar: true,
     didOpen: (card) => {
-        card.addEventListener('mouseenter', Swal.stopTimer)
-        card.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-})
+      card.addEventListener("mouseenter", Swal.stopTimer);
+      card.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
 
-Card.fire({
-    icon: 'error',
+  errorCard.fire({
+    icon: "error",
     //insert response error message
-    title: 'An error ocurred',
-    text: 'All fields are requiered'
-})
+    title: `${msg}`,
+  });
+}
 
-//Confirmation Card
-const Toast = Swal.mixin({
+function signedInCardAlert() {
+  const signedInCardAlert = Swal.mixin({
     toast: true,
-    position: 'top-end',
+    position: "top-end",
     showConfirmButton: false,
     timer: 3000,
     timerProgressBar: true,
     didOpen: (toast) => {
-      toast.addEventListener('mouseenter', Swal.stopTimer)
-      toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-  })
-  
-  Toast.fire({
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+
+  signedInCardAlert.fire({
     icon: 'success',
-    title: 'Signed in successfully' //'Successful registration"
+    title: 'Signed in successfully'
   })
+}
+
+
+function registerCardAlert(){
+  const registerCard = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  })
+
+  registerCard.fire({
+    icon: 'success',
+    title: 'Registered successfully'
+  })
+}
+
+export { errorCardAler, signedInCardAlert, registerCardAlert}
