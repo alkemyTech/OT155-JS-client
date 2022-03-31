c0a86bf675dd23696c2b28fc257ed55251f57b4d
MurabitoF <francomurabito@outlook.com>
2022-03-31 11:13:19 -0300

    Contact route added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import News from "./pages/News";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/news" element={<News />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
