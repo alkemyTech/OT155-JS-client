8e18b7edeeb153c2f9888cff48e36e46fc0f4abc
adrianlunazzi <adrianlunazzi@gmail.com>
2022-04-01 10:57:26 -0300

    add Contact page

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,78 +1,56 @@
 import React from 'react'
+import dataContacts from './dataContacts'
 
 const Contacts = () => {
-  return (
-    <div>
-        <h1>Contact page</h1>
-        <div className="flex flex-col px-12 mt-10">
-            <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                <div className="overflow-hidden">
-                    <table className="min-w-full">
-                    <thead className="border-b bg-gray-800" >
-                        <tr>
-                        <th
-                            scope="col"
-                            className="text-sm font-medium text-white px-6 py-4 text-center "
-                        >
-                            Nombre
-                        </th>
-                        <th
-                            scope="col"
-                            className="text-sm font-medium text-white px-6 py-4 text-center"
-                        >
-                            Apellido
-                        </th>
-                        <th
-                            scope="col"
-                            className="text-sm font-medium text-white px-6 py-4 text-center"
-                        >
-                            Email
-                        </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="border-b">
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">
-                            Mark
-                        </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">
-                            Otto
-                        </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">
-                            @mdo
-                        </td>
-                        </tr>
-                        <tr className="bg-white border-b">
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">
-                            Jacob
-                        </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">
-                            Thornton
-                        </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">
-                            @fat
-                        </td>
-                        </tr>
-                        <tr className="bg-white border-b">
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">
-                            Larry
-                        </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">
-                            Wild
-                        </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">
-                            @twitter
-                        </td>
-                        </tr>
-                    </tbody>
-                    </table>
-                </div>
+    return (
+        <div>
+            <h1 className="text-2xl ml-12 mt-6">Contactos</h1>
+            <div className="flex flex-col px-12 mt-10">
+                <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="overflow-hidden">
+                        <table className="min-w-full">
+                        <thead className="border-b bg-[#9AC9FB]" >
+                            <tr>
+                            <th
+                                scope="col"
+                                className="text-sm font-medium text-white px-6 py-4 text-center "
+                            >
+                                Nombre
+                            </th>
+                            <th
+                                scope="col"
+                                className="text-sm font-medium text-white px-6 py-4 text-center"
+                            >
+                                Email
+                            </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {dataContacts.map (item=>(
+                            <tr  key={item.id} className="border-b">
+                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">
+                                {item.name}
+                            </td>
+                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-center">
+                                {item.mail}
+                            </td>
+                            </tr>
+                            ))}
+                        </tbody>
+                        </table>
+                        <div className=" mt-6 flex space-x-2 justify-center">
+                            <div>
+                                <button type="button" className="inline-block px-6 py-2 border-2 mr-8 border-blue-400 text-blue-400 font-medium text-xs leading-tight uppercase rounded hover:border-blue-500 hover:text-blue-500 hover:bg-opacity-2 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">Prev</button>
+                                <button type="button" className="inline-block px-6 py-2 border-2 border-blue-400 text-blue-400 font-medium text-xs leading-tight uppercase rounded hover:border-blue-500 hover:text-blue-500 hover:bg-opacity-2 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">Next</button>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
                 </div>
             </div>
-            </div>
-    </div>
-  )
+        </div>
+      )
 }
 
 export default Contacts
