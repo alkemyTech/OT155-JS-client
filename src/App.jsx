e7f33f1c608e0234ae1685e05fc303e6d242967d
Leonardo Farina <86262814+lfarina18@users.noreply.github.com>
2022-04-05 08:37:55 -0300

    Ot155 51 - Users List (#13)
    * UserList added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { UserList } from "./components/UserList/UserList";
 import Home from "./pages/Home";
 import News from "./pages/News";
 import Register from "./pages/Register/Register";
@@ -11,6 +12,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/news" element={<News />} />
         <Route path="/register" element={<Register />}></Route>
+
+        {/* To add to private routes soon */}
+        <Route path="/backoffice/users" element={<UserList />} />
+        {/* <Route path="/backoffice/users/edituser:id" element={<UserList />} /> */}
+      
       </Routes>
     </>
 
