7e60c7266f73abea3b4c0650067e7c4428db3d4d
Franco Murabito <66337385+MurabitoF@users.noreply.github.com>
2022-04-05 10:08:18 -0300

    OT155 27 Screen Contacto (#12)
    * Contact component created
    
    * Layout finished
    
    * Fields validation and submiting resolve
    
    * Resposive design implemented
    
    * Input and textarea refactor to components
    
    * ApiConnect service applied to contact form
    
    * Contact route added
    
    * Fixing contact import

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import  Contacts from "./pages/Contacts/Contacts";
+import Contacts from "./pages/Contacts/Contacts";
 import { UserList } from "./components/UserList/UserList";
 import Home from "./pages/Home";
 import News from "./pages/News";
+import Contact from "./pages/Contact/Contact";
 import Register from "./pages/Register/Register";
 
 function App() {
@@ -12,16 +13,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/news" element={<News />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/register" element={<Register />}></Route>
         <Route path="/backoffice/contacts" element={<Contacts />}></Route>
 
         {/* To add to private routes soon */}
         <Route path="/backoffice/users" element={<UserList />} />
         {/* <Route path="/backoffice/users/edituser:id" element={<UserList />} /> */}
-      
       </Routes>
     </>
-
   );
 }
 
