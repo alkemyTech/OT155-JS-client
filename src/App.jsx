9d5070fa02379ba638b0d39e04ee83ee93bb8043
Adrian Lunazzi <74929359+adrianlunazzi@users.noreply.github.com>
2022-04-05 09:29:18 -0300

    Ot155 76 - Contact list page (#14)
    * Create contact page
    
    * add Contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import  Contacts from "./pages/Contacts/Contacts";
 import { UserList } from "./components/UserList/UserList";
 import Home from "./pages/Home";
 import News from "./pages/News";
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/news" element={<News />} />
         <Route path="/register" element={<Register />}></Route>
+        <Route path="/backoffice/contacts" element={<Contacts />}></Route>
 
         {/* To add to private routes soon */}
         <Route path="/backoffice/users" element={<UserList />} />
