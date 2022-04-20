4540500df912411badc6d51566653e5358eb8e1a
Leonardo Farina <86262814+lfarina18@users.noreply.github.com>
2022-04-20 18:46:51 -0300

    Ot155 37 - Home Edition Form (#34)
    * HomeEdit  finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+<<<<<<< HEAD
+import { Route, Routes } from 'react-router-dom';
+import ContactsList from './pages/Contacts/ContactsList';
+import { UserList } from './components/UserList/UserList';
+import Home from './pages/Home';
+import News from './pages/News/News';
+import NewsDetails from './pages/News/NewsDetails';
+import ContactForm from './pages/Contacts/ContactForm';
+import Register from './pages/Register/Register';
+import Login from '../src/pages/Login/Login';
+import EditOrganization from './pages/EditOrganization';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { HomeEdit } from './pages/Home/HomeEdit';
+=======
 import { Route, Routes } from "react-router-dom";
 import ContactsList from "./pages/Contacts/ContactsList";
 import { UserList } from "./components/UserList/UserList";
@@ -12,6 +27,7 @@ import EditOrganization from "./pages/EditOrganization";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import BackOffice from "./pages/BackOffice/BackOffice";
+>>>>>>> dev
 
 function App() {
   return (
@@ -32,6 +48,7 @@ function App() {
           <Route index element={<BackOffice />} />
           <Route path="contacts" element={<ContactsList />} />
           <Route path="edit-organization" element={<EditOrganization />} />
+          <Route path="home-edit" element={<HomeEdit />} />
           <Route path="users">
             <Route index element={<UserList />} />
             {/* <Route path=":id/edit" element={<UserList />} /> */}
