8dfd7074c4572398d0f95df204edcc7c57b5e87a
Lucía Bagnis <91097739+luciabagnis@users.noreply.github.com>
2022-04-23 20:30:46 -0300

    Detalles front (#56)
    * Redux fixing actions issues
    
    * adds token
    
    * header profile name/link | redirections | alerts

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,7 +1,10 @@
-import axios from "axios";
-import { apiConnectionWithoutToken, apiConnectionWithToken } from "../../helpers/apiConnection";
+import {
+  apiConnectionWithoutToken,
+  apiConnectionWithToken,
+} from "../../helpers/apiConnection";
+import { errorAlert, confirmationAlert } from "../../helpers/AlertService";
 
-export const loginUser = (email, password) => {
+export const loginUser = (email, password, navigate) => {
   return (dispatch) => {
     const loginValues = { email, password };
     apiConnectionWithoutToken("/users/login", loginValues, "post")
@@ -14,8 +17,9 @@ export const loginUser = (email, password) => {
             token: jwt,
           },
         });
+        navigate("/");
       })
-      .catch((err) => console.log(err.response));
+      .catch((err) => errorAlert("Error", "Contraseña o E-Mail incorrecto"));
   };
 };
 
@@ -25,43 +29,46 @@ export const logoutUser = () => {
   };
 };
 
-export const deleteUser = (id) => {
-  return async(dispatch) => {
-    await apiConnectionWithToken(`/users/${id}`, {}, 'DELETE')
-    .then(
-      dispatch({
-        type: "DELETE_USER",
-        payload: {
-          user: {},
-          token: ""
-        }
-      })
+export const deleteUser = (id, navigate) => {
+  return async (dispatch) => {
+    await apiConnectionWithToken(`/users/${id}`, {}, "DELETE")
+      .then(
+        dispatch({
+          type: "DELETE_USER",
+          payload: {
+            user: {},
+            token: "",
+          },
+        })
+      ).then(confirmationAlert("Exito", "Se ha eliminado su cuenta"))
+      .then(navigate("/"))
+      .catch((error) => console.log(error));
+  };
+};
+
+export const editUser = (firstName, lastName, email, role, id) => {
+  return async (dispatch) => {
+    await apiConnectionWithToken(
+      `/users/${id}`,
+      {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        roleId: role,
+      },
+      "PUT"
     )
+      .then(({ data }) => {
+        const { jwt, user } = data;
 
-  }
-}
-
-
-export const editUser = (firstName,lastName,email,role,id) => {
-
-  return async(dispatch) => {
-    await apiConnectionWithToken(`/users/${id}`, {
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      roleId: role
-    } , 'PUT')
-    .then(({data}) => {
-      const { jwt, user } = data;
-
-      dispatch({
-        type: "EDIT_USER",
-        payload: {
-          user: user,
-          token: jwt
-        }
+        dispatch({
+          type: "EDIT_USER",
+          payload: {
+            user: user,
+            token: jwt,
+          },
+        });
       })
-    })
-    .catch((err) => console.log(err.response))
-  }
-}
+      .catch((err) => console.log(err.response));
+  };
+};
